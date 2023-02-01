ffd0724c2892e62949918fc4e74f164be41ca0ce
Wesley McGinn <wesleymcginn1@gmail.com>
2023-02-01 10:34:08 -0500

    Made minor changes & enabled automatic resistance

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,7 @@ import frc.robot.systems.*;
 
 public class Robot extends TimedRobot {
 
-  	private final Weswerve swerveCtrl = new Weswerve(30, 31, 32, 33, 20, 21, 22, 23, 10, 11, 12, 13, 70, 100, 148, -2);
+  	private final Weswerve swerveCtrl = new Weswerve(30, 31, 32, 33, 20, 21, 22, 23, 10, 11, 12, 13, 70, 100, 148, 358);
 	private final Controls input = new Controls(0);
 	private final Navx navx = new Navx();
 	
@@ -18,6 +18,7 @@ public class Robot extends TimedRobot {
 	public double dir = 0;
 	public double dir_accuracy = 1;
 	public double rotation = 0;
+	public double resist = true;
 
 	public double deadband(double num_input, double db) {
 		if (Math.abs(num_input) < db) {
@@ -25,6 +26,16 @@ public class Robot extends TimedRobot {
 		} else {
 			return num_input;
 		}
+	}
+	
+	public void dashInit() {
+		SmartDashboard.putNumber("A Offset", swerveCtrl.A_offset);
+		SmartDashboard.putNumber("B Offset", swerveCtrl.B_offset);
+		SmartDashboard.putNumber("C Offset", swerveCtrl.C_offset);
+		SmartDashboard.putNumber("D Offset", swerveCtrl.D_offset);
+		SmartDashboard.putNumber("Speed MAX", swerveCtrl.default_speed);
+		SmartDashboard.putNumber("Steer MAX", swerveCtrl.steeringAmplifier);
+		SmartDashboard.putNumber("FRONT", front);
 	}
 
 	public void dash() {
@@ -47,10 +58,10 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putNumber("B Proportion", swerveCtrl.b);
 		SmartDashboard.putNumber("C Proportion", swerveCtrl.c);
 		SmartDashboard.putNumber("D Proportion", swerveCtrl.d);
-		SmartDashboard.putNumber("A Angle", swerveCtrl.angle0);
-		SmartDashboard.putNumber("B Angle", swerveCtrl.angle1);
-		SmartDashboard.putNumber("C Angle", swerveCtrl.angle2);
-		SmartDashboard.putNumber("D Angle", swerveCtrl.angle3);
+		SmartDashboard.putNumber("A Angle", swerveCtrl.frontLeft.getAbsolutePosition());
+		SmartDashboard.putNumber("B Angle", swerveCtrl.frontRight.getAbsolutePosition());
+		SmartDashboard.putNumber("C Angle", swerveCtrl.backRight.getAbsolutePosition());
+		SmartDashboard.putNumber("D Angle", swerveCtrl.backLeft.getAbsolutePosition());
 		SmartDashboard.putBoolean("A Negation", swerveCtrl.negation0);
 		SmartDashboard.putBoolean("B Negation", swerveCtrl.negation1);
 		SmartDashboard.putBoolean("C Negation", swerveCtrl.negation2);
@@ -59,18 +70,9 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putBoolean("Headless", headless);
 		SmartDashboard.putNumber("yaw", navx.yaw());
 		SmartDashboard.putNumber("balance", navx.balance());
+		SmartDashboard.putNumber("dir", dir);
 	}
-
-	public void dashInit() {
-		SmartDashboard.putNumber("A Offset", swerveCtrl.A_offset);
-		SmartDashboard.putNumber("B Offset", swerveCtrl.B_offset);
-		SmartDashboard.putNumber("C Offset", swerveCtrl.C_offset);
-		SmartDashboard.putNumber("D Offset", swerveCtrl.D_offset);
-		SmartDashboard.putNumber("Speed MAX", swerveCtrl.default_speed);
-		SmartDashboard.putNumber("Steer MAX", swerveCtrl.steeringAmplifier);
-		SmartDashboard.putNumber("FRONT", front);
-		SmartDashboard.putNumber("Pipeline", 0);
-	}
+	
 
 	@Override
 	public void robotInit() {
@@ -79,49 +81,42 @@ public class Robot extends TimedRobot {
 
 
 	@Override
-	public void robotPeriodic() {}
+	public void robotPeriodic() {
+		dash();
+	}
 
 
 	@Override
-	public void autonomousInit() {
-    swerveCtrl.setAngles(0, 0, 0, 0);
-  }
+	public void autonomousInit() {}
 
 
 	@Override
 	public void autonomousPeriodic() {
-    swerveCtrl.update();
-		dash();
-  }
-
-
-	@Override
-	public void testInit() {}
-
-
-	@Override
-	public void testPeriodic() {}
+		swerveCtrl.update();
+	}
 
 
 	@Override
 	public void teleopInit() {
 		navx.zeroYaw();
-		dir = 0;
 	}
 
 
 	@Override
 	public void teleopPeriodic() {
-    /*
-		dir += 2.7 * (input.in.getRawAxis(4));
-		if (Math.abs(navx.yaw()-dir) > dir_accuracy) {
-			rotation = -0.04*(navx.yaw()-dir);
+		
+		if (resist) {
+			dir += 2.7 * (input.in.getRawAxis(4));
+			if (Math.abs(navx.yaw()-dir) > dir_accuracy) {
+				rotation = -0.04*(navx.yaw()-dir);
+			} else {
+				rotation = 0;
+			}
+		} else {
+			rotation = input.in.getRawAxis(4);
 		}
-		if (Math.abs(navx.yaw()-dir) < dir_accuracy) {
-			rotation = 0;
-		}
-    */
-		SmartDashboard.putNumber("dir", dir);
+		
+		
 		if (headless) {
 			front = navx.coterminalYaw();
 			if (input.BACK.get()) { headless = false; front = 0; navx.zeroYaw(); dir = 0; }
@@ -134,8 +129,8 @@ public class Robot extends TimedRobot {
 				swerveCtrl.setAngles(front, front, front, front);
 			}
 		}
-    	rotation = input.in.getRawAxis(4);
-		if (input.RIGHT.get()) {
+    		
+		if (input.RIGHT.get()) { // Autobalance
 			swerveCtrl.speed = 0.057;
 			if (navx.balance() < -1.2) {
 				swerveCtrl.swerve(0, 1-0.05*navx.balance(), 0, 0);
@@ -150,8 +145,8 @@ public class Robot extends TimedRobot {
 			swerveCtrl.speed = swerveCtrl.default_speed + 0.8*(1-swerveCtrl.default_speed)*input.in.getRawAxis(3);
 			swerveCtrl.swerve(deadband(input.in.getRawAxis(0), 0.1), -deadband(input.in.getRawAxis(1), 0.1), rotation, front);
 		}
-    	swerveCtrl.update();
-		dash();
+		
+    		swerveCtrl.update();
 	}
 
 
@@ -163,4 +158,15 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void disabledPeriodic() {}
+	
+	
+	@Override
+	public void testInit() {
+		swerveCtrl.setAngles(0, 0, 0, 0);
+	}
+
+
+	@Override
+	public void testPeriodic() {}
+
 }
