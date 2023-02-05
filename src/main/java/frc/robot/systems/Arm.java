c79ff65ffcf0415ada2d90655c9418d116584435
NMOrlowski <wesleymcginn1@gmail.com>
2023-02-04 19:34:10 -0500

    Rewrote Arm Code

--- src/main/java/frc/robot/systems/Arm.java
+++ src/main/java/frc/robot/systems/Arm.java
@@ -1,38 +1,52 @@
 package frc.robot.systems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class Arm {
 
     private TalonSRX expansion, upDown;
-    public double expansionPosInit, upDownPosInit, expansionPos, upDownPos;
+    public double expansionPos, upDownPos;
+    private double expansionMin, expansionMax, upDownMin, upDownMax;
 
     public Arm(int expansion_canID, int upDown_canID, double expansionStart, double upDownStart) {
         expansion = new TalonSRX(expansion_canID);
         upDown = new TalonSRX(upDown_canID);
-        expansionPosInit = expansionStart;
-        upDownPosInit = upDownStart;
+        expansion.setSelectedSensorPosition(expansionStart);
+        upDown.setSelectedSensorPosition(upDownStart);
+        expansion.configOpenloopRamp(0);
+        upDown.configOpenloopRamp(0);
+        expansion.setInverted(true);
+        upDown.setInverted(false);
     }
 
-    public void setExpansion(double newPos) {
-        expansion.set(ControlMode.Position, newPos-expansionPosInit);
-        expansionPos = newPos;
+    public void setExpansion(double Rotations) {
+        expansionPos = 4096*Rotations;
+        if (expansionPos < expansionMin) { expansionPos = expansionMin; }
+        if (expansionPos > expansionMax) { expansionPos = expansionMax; }
+    }
+    
+    public void changeExpansion(double newRotations) {
+        expansionPos += 4096*newRotations;
+        if (expansionPos < expansionMin) { expansionPos = expansionMin; }
+        if (expansionPos > expansionMax) { expansionPos = expansionMax; }
     }
 
-    public void setUpDown(double newPos) {
-        upDown.set(ControlMode.Position, newPos-upDownPosInit);
-        upDownPos = newPos;
+    public void setUpDown(double Rotations) {
+        upDownPos = 4096*Rotations;
+        if (upDownPos < upDownMin) { upDownPos = upDownMin; }
+        if (upDownPos > upDownMax) { upDownPos = upDownMax; }
+    }
+    
+    public void changeUpDown(double newRotations) {
+        upDownPos += 4096*newRotations;
+        if (upDownPos < upDownMin) { upDownPos = upDownMin; }
+        if (upDownPos > upDownMax) { upDownPos = upDownMax; }
     }
 
-    public void changeExpansion(double changeBy) {
-        expansionPos += changeBy;
-        expansion.set(ControlMode.Position, expansionPos-expansionPosInit);
-    }
-
-    public void changeUpDown(double changeBy) {
-        upDownPos += changeBy;
-        upDown.set(ControlMode.Position, upDownPos-upDownPosInit);
+    public void update() {
+        expansion.set(ControlMode.PercentOutput, (expansionPos-expansion.getSelectedSensorPosition())/4096);
+        upDown.set(ControlMode.PercentOutput, (upDownPos-upDown.getSelectedSensorPosition())/4096);
     }
 
 }
