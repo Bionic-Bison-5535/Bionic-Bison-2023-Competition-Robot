ccec19da0f54570cde184ae57f3977be1deea96f
NMOrlowski <wesleymcginn1@gmail.com>
2023-02-05 20:40:34 -0500

    Reprogrammed robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,9 +12,8 @@ public class Robot extends TimedRobot {
 	private final Arm arm = new Arm(50, 51, 0, 0);
 	private final Navx navx = new Navx();
 	private final Controls primary = new Controls(0, 0.05);
-	private final Controls secondary = new Controls(0, 0.05);
+	private final Controls secondary = new Controls(1, 0.4);
 	private final GetObject collector = new GetObject(2, 1, swerveCtrl, arm);
-	private final Autobalance balancer = new Autobalance(arm, swerveCtrl, navx);
 	
 	Timer timer;
 
@@ -24,52 +23,23 @@ public class Robot extends TimedRobot {
 	public double dir_accuracy = 1;
 	public double rotation = 0;
 	public boolean resist = true;
+	public boolean rawMode = false;
+	public boolean finalMode = false;
+	private int selection;
+	private boolean selectionNegated;
+	private double computedAngle;
+	private int getting;
+	private double newAngle;
+	public int now = 0;
+	/* 0 = Driving
+	 * 1 = Adjusting Wheel
+	 * 2 = Getting Object
+	 * 3 = Preparing to Score / Aligning & Extending
+	*/
 	
-	public void dashInit() {
-		SmartDashboard.putNumber("A Offset", swerveCtrl.A_offset);
-		SmartDashboard.putNumber("B Offset", swerveCtrl.B_offset);
-		SmartDashboard.putNumber("C Offset", swerveCtrl.C_offset);
-		SmartDashboard.putNumber("D Offset", swerveCtrl.D_offset);
-		SmartDashboard.putNumber("Speed MAX", swerveCtrl.default_speed);
-		SmartDashboard.putNumber("Steer MAX", swerveCtrl.steeringAmplifier);
-		SmartDashboard.putNumber("FRONT", front);
-	}
+	public void dashInit() {}
 
-	public void dash() {
-		swerveCtrl.A_offset = SmartDashboard.getNumber("A Offset", swerveCtrl.A_offset);
-		swerveCtrl.B_offset = SmartDashboard.getNumber("B Offset", swerveCtrl.B_offset);
-		swerveCtrl.C_offset = SmartDashboard.getNumber("C Offset", swerveCtrl.C_offset);
-		swerveCtrl.D_offset = SmartDashboard.getNumber("D Offset", swerveCtrl.D_offset);
-		swerveCtrl.default_speed = SmartDashboard.getNumber("Speed MAX", swerveCtrl.default_speed);
-		swerveCtrl.steeringAmplifier = SmartDashboard.getNumber("Steer MAX", swerveCtrl.steeringAmplifier);
-		if (headless) {
-			SmartDashboard.putNumber("FRONT", front);
-		} else {
-			front = SmartDashboard.getNumber("FRONT", front);
-		}
-		SmartDashboard.putNumber("theta", swerveCtrl.theta);
-		SmartDashboard.putNumber("x", swerveCtrl.x);
-		SmartDashboard.putNumber("y", swerveCtrl.y);
-		SmartDashboard.putNumber("t", swerveCtrl.speed);
-		SmartDashboard.putNumber("A Proportion", swerveCtrl.a);
-		SmartDashboard.putNumber("B Proportion", swerveCtrl.b);
-		SmartDashboard.putNumber("C Proportion", swerveCtrl.c);
-		SmartDashboard.putNumber("D Proportion", swerveCtrl.d);
-		SmartDashboard.putNumber("A Angle", swerveCtrl.frontLeft.getAbsolutePosition());
-		SmartDashboard.putNumber("B Angle", swerveCtrl.frontRight.getAbsolutePosition());
-		SmartDashboard.putNumber("C Angle", swerveCtrl.backRight.getAbsolutePosition());
-		SmartDashboard.putNumber("D Angle", swerveCtrl.backLeft.getAbsolutePosition());
-		SmartDashboard.putBoolean("A Negation", swerveCtrl.negation0);
-		SmartDashboard.putBoolean("B Negation", swerveCtrl.negation1);
-		SmartDashboard.putBoolean("C Negation", swerveCtrl.negation2);
-		SmartDashboard.putBoolean("D Negation", swerveCtrl.negation3);
-		SmartDashboard.putBoolean("Moving Enabled", swerveCtrl.move);
-		SmartDashboard.putBoolean("Headless", headless);
-		SmartDashboard.putNumber("yaw", navx.yaw());
-		SmartDashboard.putNumber("balance", navx.balance());
-		SmartDashboard.putNumber("dir", dir);
-		SmartDashboard.putBoolean("resistance", resist);
-	}
+	public void dash() {}
 	
 
 	@Override
@@ -89,70 +59,154 @@ public class Robot extends TimedRobot {
 
 
 	@Override
-	public void autonomousPeriodic() {}
+	public void autonomousPeriodic() {
+		swerveCtrl.update();
+		arm.update();
+	}
 
 
 	@Override
 	public void teleopInit() {
 		navx.zeroYaw();
+		dir = 0;
 	}
 
 
 	@Override
 	public void teleopPeriodic() {
-		
-		if (resist) {
-			dir += 3 * (primary.stick(3)-primary.stick(2));
-			if (Math.abs(navx.yaw()-dir) > dir_accuracy) {
-				rotation = -0.04*(navx.yaw()-dir);
+
+		// Secondary Controller Input:
+		if (secondary.BACK.get()) {
+			rawMode = true;
+		}
+		if (secondary.START.get()) {
+			rawMode = false;
+		}
+		if (secondary.LEFT.get()) {
+			finalMode = true;
+		}
+		if (secondary.RIGHT.get()) {
+			finalMode = false;
+		}
+		if (secondary.A.get()) {
+			resist = true;
+		}
+		if (secondary.B.get()) {
+			resist = false;
+		}
+		if (secondary.pov() != -1) {
+			now = 1;
+			selection = secondary.pov();
+			swerveCtrl.setAngles(0, 0, 0, 0);
+			swerveCtrl.setVelocities(0, 0, 0, 0);
+		}
+		if (secondary.Y.get()) {
+			now = 0;
+		}
+
+		// Dynamic Periodics:
+		if (now == 0) {
+			if (rawMode) {
+				swerveCtrl.swerve(-primary.stick(1), primary.stick(0), primary.stick(3)-primary.stick(2), 0);
+				arm.changeExpansion(primary.stick(4));
+				arm.changeUpDown(primary.stick(5));
 			} else {
-				rotation = 0;
+				if (finalMode) {
+					swerveCtrl.speed = swerveCtrl.default_speed * 0.47;
+					if (primary.B.get()) {
+						if (navx.balance() < -2 && arm.there) {
+							arm.changeExpansion(2);
+						}
+						if (navx.balance() > 2 && arm.there) {
+							arm.changeExpansion(-2);
+						}
+					}
+				} else {
+					swerveCtrl.speed = swerveCtrl.default_speed;
+					if (primary.X.get()) {
+						getting = 0;
+						now = 2;
+					}
+					if (primary.Y.get()) {
+						getting = 1;
+						now = 2;
+					}
+				}
+				if (primary.A.get()) {
+					navx.zeroYaw();
+					dir = 0;
+				}
+				if (primary.LEFT.get()) {
+					if (primary.stick(0) >= 0) {
+						dir -= 180;
+					} else {
+						dir += 180;
+					}
+				}
+				if (primary.pov() != -1) {
+					newAngle = (double)primary.pov();
+					while (newAngle > dir+180) { newAngle -= 360; }
+					while (newAngle < dir-180) { newAngle += 360; }
+					dir = newAngle;
+				}
+				if (resist) {
+					dir += 3 * (primary.stick(3)-primary.stick(2));
+					if (Math.abs(navx.yaw()-dir) > dir_accuracy) {
+						rotation = -0.04*(navx.yaw()-dir);
+					} else {
+						rotation = 0;
+					}
+				} else {
+					rotation = (primary.stick(3)-primary.stick(2));
+				}
+				if (primary.BACK.get()) {
+					headless = false;
+				}
+				if (primary.START.get()) {
+					headless = true;
+				}
+				if (headless) {
+					swerveCtrl.swerve(-primary.stick(1), primary.stick(0), rotation, navx.coterminalYaw());
+				} else {
+					swerveCtrl.swerve(-primary.stick(1), primary.stick(0), rotation, 0);
+				}
 			}
-		} else {
-			rotation = (primary.stick(3)-primary.stick(2));
 		}
-		
-		if (headless) {
-			front = navx.coterminalYaw();
-			if (primary.BACK.get()) { headless = false; front = 0; navx.zeroYaw(); dir = 0; }
-			if (primary.LEFT.get()) { navx.zeroYaw(); rotation = 0; dir = 0; }
-		} else {
-			if (primary.START.get()) { headless = true; navx.zeroYaw(); dir = 0; rotation = 0; }
-			if (primary.in.getPOV() != -1) {
-				front = -primary.in.getPOV();
-				SmartDashboard.putNumber("FRONT", front);
-				swerveCtrl.setAngles(front, front, front, front);
+		if (now == 1) {
+			if (secondary.X.get()) {
+				selectionNegated = !selectionNegated;
+				if (selectionNegated) {
+					if (selection == 0) { swerveCtrl.setVelocities(-0.4, 0, 0, 0); }
+					if (selection == 90) { swerveCtrl.setVelocities(0, -0.4, 0, 0); }
+					if (selection == 180) { swerveCtrl.setVelocities(0, 0, -0.4, 0); }
+					if (selection == 270) { swerveCtrl.setVelocities(0, 0, 0, -0.4); }
+				} else {
+					if (selection == 0) { swerveCtrl.setVelocities(0.4, 0, 0, 0); }
+					if (selection == 90) { swerveCtrl.setVelocities(0, 0.4, 0, 0); }
+					if (selection == 180) { swerveCtrl.setVelocities(0, 0, 0.4, 0); }
+					if (selection == 270) { swerveCtrl.setVelocities(0, 0, 0, 0.4); }
+				}
+				while (secondary.X.get()) {}
+				swerveCtrl.setVelocities(0, 0, 0, 0);
+			}
+			computedAngle = swerveCtrl.i_tan(secondary.stick(0), secondary.stick(1));
+			if (selectionNegated) { computedAngle += 180; }
+			if (selection == 0) { swerveCtrl.A_offset = computedAngle; }
+			if (selection == 90) { swerveCtrl.B_offset = computedAngle; }
+			if (selection == 180) { swerveCtrl.C_offset = computedAngle; }
+			if (selection == 270) { swerveCtrl.D_offset = computedAngle; }
+		}
+		if (now == 2) {
+			if (collector.getGamePiece(getting)) {
+				now = 0;
 			}
 		}
-    		
-		if (primary.RIGHT.get()) { // Autobalance
-			swerveCtrl.speed = 0.057;
-			if (navx.balance() < -1.2) {
-				swerveCtrl.swerve(0, 1-0.05*navx.balance(), 0, 0);
-			}
-			if (navx.balance() > 1.2) {
-				swerveCtrl.swerve(0, -1-0.05*navx.balance(), 0, 0);
-			}
-			if (navx.balance() >= -1.2 && navx.balance() <= 1.2) {
-				swerveCtrl.swerve(0, 0, 0, 0);
-			}
-		} else {
-			swerveCtrl.speed = swerveCtrl.default_speed + 0.8*(1-swerveCtrl.default_speed)*primary.stick(3);
-			swerveCtrl.swerve(-primary.stick(1), primary.stick(0), rotation, front);
-		}
-		
-    	swerveCtrl.update();
+		if (now == 4) {}
+
+		// Static Periodics:
+		swerveCtrl.update();
+		arm.update();
 	}
-
-
-	@Override
-	public void disabledInit() {
-		swerveCtrl.setVelocities(0, 0, 0, 0);
-	}
-
-
-	@Override
-	public void disabledPeriodic() {}
 	
 	
 	@Override
