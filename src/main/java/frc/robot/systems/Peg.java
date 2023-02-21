28651f0ef2d022b5aa4cb9e386a37380d2c8b1a1
Wesley McGinn <wesleymcginn1@gmail.com>
2023-02-20 21:15:47 -0500

    Added peg system

---
+++ src/main/java/frc/robot/systems/Peg.java
@@ -0,0 +1,41 @@
+package frc.robot.systems;
+
+import edu.wpi.first.wpilibj.PWM;
+
+public class Peg {
+
+    private PWM peg1, peg2;
+    public double in_pos, out_pos;
+    public boolean out = false;
+
+    public Peg(int peg1PWMport, int peg2PWMPort, double inPosition, double outPosition) {
+        peg1 = new PWM(peg1PWMport);
+        peg2 = new PWM(peg2PWMPort);
+        in_pos = inPosition;
+        out_pos = outPosition;
+        in();
+    }
+
+    public void in() {
+        peg1.setSpeed(in_pos);
+        peg2.setSpeed(in_pos);
+        out = false;
+    }
+
+    public void out() {
+        peg1.setSpeed(out_pos);
+        peg2.setSpeed(in_pos);
+        out = true;
+    }
+
+    public void pos(double position) {
+        peg1.setSpeed(position);
+        peg2.setSpeed(position);
+        if (position < 0) {
+            out = false;
+        } else {
+            out = true;
+        }
+    }
+
+}
