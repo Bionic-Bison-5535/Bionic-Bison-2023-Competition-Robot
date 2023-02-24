4efe1638f121b2b77bd82c0d7ad322d943e299ec
Wesley McGinn <wesleymcginn1@gmail.com>
2023-02-24 11:56:45 -0500

    Added Encoder Support

--- src/main/java/frc/robot/systems/Motor.java
+++ src/main/java/frc/robot/systems/Motor.java
@@ -14,17 +14,47 @@ public class Motor {
 
     public Motor(int canID, boolean isTalon) {
         if (isTalon) {
+        usingTalon = isTalon;
             talonMotor = new TalonSRX(canID);
         } else {
             maxMotor = new CANSparkMax(canID, MotorType.kBrushless);
+            canEncoder = maxMotor.getEncoder();
         }
     }
 
     public void set(double power) {
-        if (isTalon) {
+        if (usingTalon) {
             talonMotor.set(ControlMode.PercentOutput, power);
         } else {
             maxMotor.set(power);
+        }
+    }
+
+    public double getEnc() {
+        if (usingTalon) {
+            return talonMotor.getSelectedSensorPosition();
+        } else {
+            return canEncoder.getPosition();
+        }
+    }
+
+    public double getRotations() {
+        return (getEnc() / ticksPerRotation());
+    }
+
+    public void setEnc(double newEncValue) {
+        if (usingTalon) {
+            talonMotor.setSelectedSensorPosition(newEncValue);
+        } else {
+            canEncoder.setPosition(newEncValue);
+        }
+    }
+
+    public double ticksPerRotation() {
+        if (usingTalon) {
+            return 4096;
+        } else {
+            return 43;
         }
     }
 
