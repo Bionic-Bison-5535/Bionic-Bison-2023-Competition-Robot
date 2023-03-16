a1d76d90f030c9320f4b451f02dc19f49737ad01
Wesley McGinn <wesleymcginn1@gmail.com>
2023-03-16 19:25:55 -0400

    Fix Arm Positions

--- src/main/java/frc/robot/systems/Arm.java
+++ src/main/java/frc/robot/systems/Arm.java
@@ -4,21 +4,23 @@ public class Arm {
 
     public Motor alpha, beta;
 
-    public double pos_0_a = 0; // Position 0 - Collect or Score in Row 1
-    public double pos_0_b = 0;
+    public double pos_0_a = 62; // Position 0 - Collect or Score in Row 1
+    public double pos_0_b = 60000;
     public double pos_1_a = 0; // Position 1 -  Score in Row 2
-    public double pos_1_b = 0;
-    public double pos_2_a = 100; // Position 2 - Score in Row 3 (Most extended and most dangerous)
-    public double pos_2_b = 1000;
+    public double pos_1_b = 114000;
+    public double pos_2_a = -52; // Position 2 - Score in Row 3 (Most extended and most dangerous)
+    public double pos_2_b = 114000;
     public double pos_3_a = 0; // Position 3 - Holding Mode
-    public double pos_3_b = 0;
+    public double pos_3_b = 3500;
 
     public int previousPos = 3;
     public int mostRecentPos = 3;
 
     public Arm(int alpha_canID, int beta_canID) {
         alpha = new Motor(alpha_canID, false, false, 1);
-        beta = new Motor(beta_canID, true, false, 1);
+        beta = new Motor(beta_canID, true, false, 0.5);
+        alpha.db = 0.1;
+        beta.db = 0.07;
         alpha.setEnc(0);
         beta.setEnc(0);
     }
@@ -29,8 +31,6 @@ public class Arm {
     }
 
     public void pos(int positionNumber) {
-        previousPos = mostRecentPos;
-        mostRecentPos = positionNumber;
         if (positionNumber == 0) {
             go(pos_0_a, pos_0_b);
         }
@@ -43,6 +43,11 @@ public class Arm {
         if (positionNumber == 3) {
             go(pos_3_a, pos_3_b);
         }
+        if (positionNumber == 4) {
+            go(pos_3_a, pos_0_b);
+        }
+        previousPos = mostRecentPos;
+        mostRecentPos = positionNumber;
     }
 
     public boolean all_there() {
