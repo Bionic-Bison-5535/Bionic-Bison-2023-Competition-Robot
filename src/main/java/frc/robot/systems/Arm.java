928218415d9fdb893c7c78f5bcd8636fcc6df43a
Wesley McGinn <wesleymcginn1@gmail.com>
2023-03-18 14:58:32 -0400

    Fix Arm System

--- src/main/java/frc/robot/systems/Arm.java
+++ src/main/java/frc/robot/systems/Arm.java
@@ -4,23 +4,23 @@ public class Arm {
 
     public Motor alpha, beta;
 
-    public double pos_0_a = 62; // Position 0 - Collect or Score in Row 1
+    public double pos_0_a = 57; // Position 0 - Collect or Score in Row 1
     public double pos_0_b = 60000;
     public double pos_1_a = 0; // Position 1 -  Score in Row 2
     public double pos_1_b = 80000;
-    public double pos_2_a = -52; // Position 2 - Score in Row 3 (Most extended and most dangerous)
+    public double pos_2_a = -40; // Position 2 - Score in Row 3 (Most extended and most dangerous)
     public double pos_2_b = 114000;
     public double pos_3_a = 0; // Position 3 - Holding Mode
-    public double pos_3_b = 4000;
+    public double pos_3_b = 5500;
 
     public int previousPos = 3;
     public int mostRecentPos = 3;
 
+    private int special = 0;
+
     public Arm(int alpha_canID, int beta_canID) {
         alpha = new Motor(alpha_canID, false, false, 1);
         beta = new Motor(beta_canID, true, false, 0.5);
-        alpha.db = 0.1;
-        beta.db = 0.07;
         alpha.setEnc(0);
         beta.setEnc(0);
     }
@@ -28,26 +28,52 @@ public class Arm {
     public void go(double alpha_encValue, double beta_encValue) {
         alpha.goTo(alpha_encValue);
         beta.goTo(beta_encValue);
+        alpha.db = 0.1;
     }
 
     public void pos(int positionNumber) {
-        if (positionNumber == 0) {
-            go(pos_0_a, pos_0_b);
+        if (previousPos == 3 && positionNumber == 0) {
+            special = -1;
+        } else if (previousPos == 0 && positionNumber == 3) {
+            special = 1;
+        } else {
+            special = 0;
         }
-        if (positionNumber == 1) {
-            go(pos_1_a, pos_1_b);
-        }
-        if (positionNumber == 2) {
-            go(pos_2_a, pos_2_b);
-        }
-        if (positionNumber == 3) {
-            go(pos_3_a, pos_3_b);
-        }
-        if (positionNumber == 4) {
+        if (special == 0) {
+            if (positionNumber == 0) {
+                go(pos_0_a, pos_0_b);
+            } else if (positionNumber == 1) {
+                go(pos_1_a, pos_1_b);
+            } else if (positionNumber == 2) {
+                go(pos_2_a, pos_2_b);
+            } else if (positionNumber == 3) {
+                go(pos_3_a, pos_3_b);
+            } else if (positionNumber == 4) {
+                go(pos_3_a, pos_0_b);
+            }
+            previousPos = mostRecentPos;
+            mostRecentPos = positionNumber;
+        } else if (special == -1) {
             go(pos_3_a, pos_0_b);
+            if (beta.almost()) {
+                go(pos_0_a, pos_0_b);
+                if (alpha.almost()) {
+                    special = 0;
+                    previousPos = 0;
+                    mostRecentPos = 0;
+                }
+            }
+        } else if (special == 1) {
+            go(pos_3_a, pos_0_b);
+            if (alpha.almost()) {
+                go(pos_3_a, pos_3_b);
+                if (alpha.almost()) {
+                    special = 0;
+                    previousPos = 3;
+                    mostRecentPos = 3;
+                }
+            }
         }
-        previousPos = mostRecentPos;
-        mostRecentPos = positionNumber;
     }
 
     public boolean all_there() {
