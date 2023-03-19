e266700c6c77ad2902d847276888a28ca47ed65a
Wesley McGinn <wesleymcginn1@gmail.com>
2023-03-19 18:18:43 -0400

    Make GetObject Smart Feature FMS-Based
    Previously, the robot relied on `counts`, basically taking the number of times the periodic loop had run as a timer in itself.  This was not a good strategy as the robot's thinking speed was not always consistent.  Now, the robot relies on the FMS timer to keep track of timeouts and waiting periods.

--- src/main/java/frc/robot/smart_features/GetObject.java
+++ src/main/java/frc/robot/smart_features/GetObject.java
@@ -1,5 +1,6 @@
 package frc.robot.smart_features;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.systems.Weswerve;
 import frc.robot.systems.Limelight;
 import frc.robot.systems.Arm;
@@ -11,16 +12,28 @@ public class GetObject {
     private Weswerve swerveCtrl;
     private Arm arm;
     private Intake claw;
-    private int counts = 0;
+    private double time = 0;
+    private double startTime = 0;
     public int stage = 0; // 0 = Ready, 1 = Arm Out, 2 = Arm Down, 3 = Alignment, 4 = Drive Forward, 5 = Arm Up, 6 = Arm In,
 
-    public double cubeWidthForPickUp = 150;
+    public double cubeWidthForPickUp = 130;
 
     public GetObject(int cubePipeline, Weswerve swerveAccess, Arm armAccess, Intake intakeAccess) {
         cubeCam = new Limelight(cubePipeline);
         swerveCtrl = swerveAccess;
         arm = armAccess;
         claw = intakeAccess;
+    }
+
+    private void nextStage(boolean transitionIf, double timeout) {
+        if (transitionIf || startTime - timeout <= time) {
+            startTime = DriverStation.getMatchTime();
+            stage++;
+        }
+    }
+
+    private void staticPeriodic() {
+        time = DriverStation.getMatchTime();
     }
 
     public boolean alignToCube() {
@@ -35,52 +48,40 @@ public class GetObject {
             }
         } else {
             swerveCtrl.swerve(0, 0, 0, 0);
+            stage = 6;
             return true;
         }
     }
 
     public boolean getGamePiece() { // Returns true if done, otherwise must be run periodically
-        counts += 1;
-        if (stage == 0) {
-            counts = 0;
-            stage = 1;
+        staticPeriodic();
+        if (stage <= 0) {
+            nextStage(true, 0);
         }
         if (stage == 1) {
             arm.pos(4);
-            if (arm.all_there()) {
-                stage++;
-            }
+            nextStage(arm.all_there(), 3);
         }
         if (stage == 2) {
             arm.pos(0);
-            if (arm.all_there()) {
-                stage++;
-                counts = 0;
-            }
+            nextStage(arm.all_there(), 3);
         }
         if (stage == 3) {
             claw.take();
-            if (alignToCube() || counts > 50) {
-                stage++;
-                counts = 0;
-            }
+            nextStage(alignToCube(), 4);
         }
         if (stage == 4) {
             claw.take();
-            swerveCtrl.swerve(0.2, 0, 0, 0);
-            if (counts > 30) {
-                stage++;
-            }
+            swerveCtrl.swerve(0.25, 0, 0, 0);
+            nextStage(false, 1.5);
         }
         if (stage == 5) {
-            swerveCtrl.swerve(0, 0, 0, 0);
             claw.stop();
+            swerveCtrl.swerve(0, 0, 0, 0);
             arm.pos(4);
-            if (arm.all_there()) {
-                stage++;
-            }
+            nextStage(arm.all_there(), 3);
         }
-        if (stage == 6) {
+        if (stage >= 6) {
             swerveCtrl.swerve(0, 0, 0, 0);
             claw.stop();
             arm.pos(3);
