6e1ef85b9adcd0c6097928f2252ae2d8512956c5
Wesley McGinn <wesleymcginn1@gmail.com>
2023-03-23 10:03:41 -0400

    Finish Programming Autoalign

--- src/main/java/frc/robot/smart_features/Score.java
+++ src/main/java/frc/robot/smart_features/Score.java
@@ -16,8 +16,8 @@ public class Score {
     private Navx navx;
     private boolean auto, tele;
     private double time = 120;
-    private double lastScored = -1;
-    public int stage = 0; // 0 = Ready, 1 = Aligning Horizontally, 2 = Pressing Up Against Grid, 3 = Adjusting Arm Position, 4 = Done + Arm Up, 5 = Error / Cancel,
+    private double lastScored = 200;
+    public int stage = 0; // 0 = Ready, 1 = Aligning Horizontally, 2 = Pressing Up Against Grid, 3 = Done + Arm Up
 
     public int cones = 0;
     public int cubes = 0;
@@ -31,69 +31,56 @@ public class Score {
         navx = navxAccess;
     }
 
-    public boolean align() {
+    private void nextStage(boolean transitionIf) {
+        if (transitionIf) {
+            stage++;
+        }
+    }
+
+    public boolean alignHorizontal() {
         if (april.valid()) {
-            if (april.inRange(april.width(), cubeWidthForPickUp, 30) && april.inRange(april.X(), 0, 7)) {
-                stage = 2;
-                swerveCtrl.swerve(0, 0, 0, 0);
+            if (april.inRange(april.X(), 0, 8)) {
+                swerveCtrl.lock();
                 return true;
             } else {
-                swerveCtrl.swerve((cubeWidthForPickUp-april.width())/90, april.X()/50, 0, 0);
+                swerveCtrl.swerve(0, april.X()/40, -0.02*navx.yaw(), 0);
                 return false;
             }
         } else {
-            swerveCtrl.swerve(0, 0, 0, 0);
+            swerveCtrl.lock();
             return true;
         }
     }
 
-    public boolean getGamePiece() { // Returns true if done, otherwise must be run periodically
-        counts += 1;
+    public boolean alignVertical() {
+        if (april.valid()) {
+            if (april.Y() >= setHeight) {
+                swerveCtrl.lock();
+                return true;
+            } else {
+                swerveCtrl.swerve(0.15, -0.02*navx.yaw(), 0);
+                return false;
+            }
+        } else {
+            swerveCtrl.lock();
+            return true;
+        }
+    }
+
+    public boolean run() { // Returns true if done, otherwise must be run periodically
         if (stage == 0) {
-            counts = 0;
             stage = 1;
         }
         if (stage == 1) {
-            arm.pos(4);
-            if (arm.all_there()) {
-                stage++;
-            }
+            nextStage(alignHorizontal());
         }
         if (stage == 2) {
-            arm.pos(0);
-            if (arm.all_there()) {
-                stage++;
-                counts = 0;
-            }
+            nextStage(alignVertical());
         }
-        if (stage == 3) {
-            claw.take();
-            if (alignToCube() || counts > 170) {
-                stage++;
-                counts = 0;
-            }
-        }
-        if (stage == 4) {
-            claw.take();
-            swerveCtrl.swerve(0.2, 0, 0, 0);
-            if (counts > 30) {
-                stage++;
-            }
-        }
-        if (stage == 5) {
-            swerveCtrl.swerve(0, 0, 0, 0);
-            claw.stop();
-            arm.pos(4);
-            if (arm.all_there()) {
-                stage++;
-            }
-        }
-        if (stage == 6) {
-            swerveCtrl.swerve(0, 0, 0, 0);
-            claw.stop();
-            arm.pos(3);
+        if (stage >= 3) {
+            swerveCtrl.lock();
+            arm.pos(1);
             stage = 0;
-            counts = 0;
             return true;
         } else {
             return false;
